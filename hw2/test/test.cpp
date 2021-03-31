d2fe377ef129997e68832ae7d93857bbf7c76d0f
Ksenia <ksju.nikitina@rambler.ru>
2021-03-31 17:10:44 +0300

    check

--- hw2/test/test.cpp
+++ hw2/test/test.cpp
@@ -40,12 +40,14 @@ TEST(CalculateSum, CorrectArgs) {
 
 TEST(CalculateSum, CorrectSum1) {
     Matrix *m = create_matrix_from_file("../in");
-    for (int i =0;i<m->rows;i++) {
+    if (!m)
+        printf("ERROR");
+    /*for (int i =0;i<m->rows;i++) {
         for (int j = 0; j < m->cols; j++) {
             printf("%lf ", m->ptr[i * m->cols + j]);
         }
         printf("\n");
-    }
+    }*/
     double total_sum = 0;
     calculate_sum(m, &total_sum);
     ASSERT_NEAR(total_sum, 57.19, 1e-6);
